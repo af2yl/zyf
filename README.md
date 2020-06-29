91f5826c5c362d952cd85e0fe8f91a6f936d8392
af2yl <1820023574@qq.com>
2020-06-29 22:39:47 +0800

    jkfd

---
+++ README.md
@@ -0,0 +1,2 @@
+# Demozyf
+Demoaf
---
+++ src/AF/tfpoint.java
@@ -0,0 +1,14 @@
+package AF;
+public class tfpoint{
+    public static void main(String[] args) {
+        System.out.println(solve24(1,2,3,4));
+        int [] a={1,5,5,5};
+        String[] op={"*","-","/"};
+
+    }
+
+    public static String solve24(int a, int b, int c, int d) {
+        //TODO;
+        return "1*2*3*4=24";
+    }
+}
